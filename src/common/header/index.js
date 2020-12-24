97aebef6eb2f12a918ac805f29a93fc1f516fee4
mluckydream <2274607609@qq.com>
2020-12-24 16:02:05 +0800

    feat: 优化代码，避免无意义ajax请求

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -59,7 +59,7 @@ class Header extends Component {
     }
 
     render(){
-        const { focused, handleInputFocus, handleInputBlur } = this.props;
+        const { focused, handleInputFocus, handleInputBlur, list } = this.props;
         return(
             <HeaderWrapper>
                 <IconFontStyle />
@@ -79,7 +79,7 @@ class Header extends Component {
                         >
                             <NavSearch 
                             className={focused ? 'focused' : ''}
-                            onFocus={handleInputFocus}
+                            onFocus={() => handleInputFocus(list)}
                             onBlur={handleInputBlur}
                             ></NavSearch>
                         </Transition>
@@ -120,8 +120,8 @@ const mapStateToProps = (state) => {
 
 const mapDispathToProps = (dispath) => {
     return {
-        handleInputFocus(){
-            dispath(actionCreators.getList());
+        handleInputFocus(list){
+           (list.size === 0) && dispath(actionCreators.getList());
             dispath(actionCreators.searchFocus());
         },
         handleInputBlur(){
---
+++ src/pages/home/components/Recommend.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Recommend extends Component {
+    render(){
+        return (
+           <div> Commend </div> 
+        )
+    }
+}
+export default Recommend;
